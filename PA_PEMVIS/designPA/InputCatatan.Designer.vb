5c31d02485e6b5c4a2507135d8c377b17be3ca4f
Aiurskiesyx <145420096+Aiurskiesyx@users.noreply.github.com>
2024-05-17 22:08:44 +0800

    PA_PEMVIS_B1K6

---
+++ PA_PEMVIS/designPA/InputCatatan.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class InputCatatan
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(235, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(232, Byte), Integer))
+        Me.TextBox1.Font = New System.Drawing.Font("Rockwell", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(56, 92)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(652, 368)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(210, Byte), Integer), CType(CType(227, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(668, 486)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 38)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Simpan"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Rockwell", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(128, 30)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(518, 44)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "TAMBAH CATATAN PASIEN"
+        '
+        'btnBack
+        '
+        Me.btnBack.BackgroundImage = Global.designPA.My.Resources.Resources.icons8_back_48
+        Me.btnBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.btnBack.FlatAppearance.BorderSize = 0
+        Me.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnBack.Location = New System.Drawing.Point(12, 478)
+        Me.btnBack.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(57, 54)
+        Me.btnBack.TabIndex = 8
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'InputCatatan
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(178, Byte), Integer), CType(CType(200, Byte), Integer), CType(CType(186, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(796, 550)
+        Me.Controls.Add(Me.btnBack)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "InputCatatan"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "InputCatatan"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents btnBack As System.Windows.Forms.Button
+End Class
---
+++ PA_PEMVIS/designPA/Login.vb
@@ -0,0 +1,84 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Login
+    Public Shared globalId As Integer
+
+    Sub Kosong()
+        txtPw.Clear()
+        txtUsn.Clear()
+        txtUsn.Focus()
+        txtPw.Focus()
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim username As String = txtUsn.Text.Trim()
+        Dim password As String = txtPw.Text.Trim()
+
+        Try
+            If username.ToLower() = "admin" AndAlso password = "123" Then
+                loginSuccess("admin")
+            Else
+                Dim id As Integer = authenticateUser(username, password)
+                If id <> 0 Then
+                    globalId = id
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox("Terjadi kesalahan: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Function authenticateUser(username As String, password As String) As Integer
+        Dim id As Integer = 0
+        koneksi()
+            Dim query As String = "SELECT id FROM tbakun WHERE username = @username AND password = @password"
+            Using CMD As New MySqlCommand(query, CONN)
+                CMD.Parameters.AddWithValue("@username", username)
+                CMD.Parameters.AddWithValue("@password", password)
+                Using RD As MySqlDataReader = CMD.ExecuteReader()
+                    If RD.Read() Then
+                    id = RD.GetInt32(0)
+                        loginSuccess("pasien")
+                    Else
+                        MsgBox("Username atau password salah.")
+                    End If
+                End Using
+            End Using
+        Return id
+    End Function
+
+    Private Sub loginSuccess(role As String)
+        If role = "admin" Then
+            MenuAdmin.Show()
+        ElseIf role = "pasien" Then
+            MenuPasien.username.Text = txtUsn.Text()
+            JanjiTemuPasien.username.Text = txtUsn.Text()
+            MenuPasien.Show()
+        Else
+            MsgBox("Tidak ada peran yang cocok untuk pengguna ini.")
+        End If
+        Me.Hide()
+    End Sub
+
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Kosong()
+    End Sub
+
+    Private Sub btnRegis_Click(sender As Object, e As EventArgs) Handles btnRegis.Click
+        Me.Hide()
+        Registrasi.Show()
+        Kosong()
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Dispose()
+    End Sub
+
+    Private Sub cbShowPass_CheckedChanged(sender As Object, e As EventArgs) Handles cbShowPass.CheckedChanged
+        If cbShowPass.Checked Then
+            txtPw.PasswordChar = ControlChars.NullChar
+        Else
+            txtPw.PasswordChar = "*"c
+        End If
+    End Sub
+End Class
